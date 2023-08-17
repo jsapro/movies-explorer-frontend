c4b83d36a92c90434549fd1e603761e493005a2c
jsapro <77.3.77@mail.ru>
2023-08-17 15:23:42 +0300

    add styles to FilterCheckbox component

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -2,9 +2,9 @@ import './FilterCheckbox.css';
 
 const FilterCheckbox = () => {
   return (
-    <label htmlFor='checkbox'>
-      <input id='checkbox' type='checkbox'></input>
-      Короткометражки
+    <label className='filter-checkbox' htmlFor='checkbox'>
+      <input className='filter-checkbox__input' id='checkbox' type='checkbox'></input>
+      <span className='filter-checkbox__text'>Короткометражки</span>
     </label>
   );
 };
