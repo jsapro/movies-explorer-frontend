46b76b21010baef0c542012736d5e18edc511df9
jsapro <77.3.77@mail.ru>
2023-08-18 13:35:04 +0300

    add button for loading more movies

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -3,14 +3,20 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 
 const MoviesCardList = () => {
   return (
-    <ul className='movies-card-list'>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-      <MoviesCard/>
-    </ul>
+    <>
+      <ul className='movies-card-list'>
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+        <MoviesCard />
+      </ul>
+
+      <div className='movies-card-list__btn-container'>
+        <button className='movies-card-list__more-btn' type='button' disabled='false'>Ещё</button>
+      </div>
+    </>
   );
 };
 
