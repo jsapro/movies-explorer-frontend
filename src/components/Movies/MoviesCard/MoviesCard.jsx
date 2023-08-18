a98d36ef5bcfd7c7b8d3f9884c7294799e087dbb
jsapro <77.3.77@mail.ru>
2023-08-18 10:54:40 +0300

    add styles to MoviesCard component

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,11 +1,32 @@
 import './MoviesCard.css';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const MoviesCard = () => {
+
+
+  // =======================================
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.nomoreparties.co/beatfilm-movies')
+      .then((data) => data.json())
+      .then((data) => {
+        setMovieList(data);
+      })
+      .catch((err) => console.log(`ошибка fetch запроса: ${err.message}`));
+  }, []);
+
+  console.log('movieList', movieList[10]);
+  // =======================================
+
+  const fotoUrl =
+    'https://cdn.stocksnap.io/img-thumbs/960w/colorful-boats_C42YPG8SOM.jpg';
+  
   const location = useLocation();
 
-  const isSaved = true;
-  // const isSaved = false;
+  // const isSaved = true;
+  const isSaved = false;
 
   const handleBtnClick = () => {
     if (location.pathname === '/saved-movies') return () => {};
@@ -31,12 +52,12 @@ const MoviesCard = () => {
           type='button'
         />
         <Link className='movies-card__link' to='#' target='_blank'>
-          <img className='movies-card__img' src={'card.link'} alt='' />
+          <img className='movies-card__img' src={fotoUrl} alt='' />
         </Link>
       </div>
       <div className='movies-card__caption'>
-        <h2 className='movies-card__name'>name</h2>
-        <p className='movies-card__duration'>duration</p>
+        <h2 className='movies-card__name'>В погоне за Бенкси</h2>
+        <p className='movies-card__duration'>1ч 17м</p>
       </div>
     </li>
   );
