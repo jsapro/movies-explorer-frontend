dd502cad70ec5e9d07146a59b4c05bd56f0fe5bb
jsapro <77.3.77@mail.ru>
2023-08-18 12:01:13 +0300

    add structure to saved-movies page

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,9 +1,18 @@
 import './SavedMovies.css';
+import MoviesCardList from '../Movies/MoviesCardList/MoviesCardList';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import SearchForm from '../Movies/SearchForm/SearchForm';
 
 const SavedMovies = () => {
   return (
     <>
-      <div>SavedMovies is imported !!!!!</div>
+      <Header />
+      <main>
+        <SearchForm />
+        <MoviesCardList />
+      </main>
+      <Footer />
     </>
   );
 };
