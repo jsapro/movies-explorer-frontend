4911293ead743a6d375d2de490dfaa58a9ead36e
jsapro <77.3.77@mail.ru>
2023-08-19 23:29:51 +0300

    add styles to Profile component on 320px

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,8 @@ import './Profile.css';
 import Header from '../Header/Header';
 
 const Profile = () => {
-  const isReadyToSave = true;
-  // const isReadyToSave = false;
+  // const isReadyToSave = true;
+  const isReadyToSave = false;
 
   return (
     <>
@@ -13,7 +13,7 @@ const Profile = () => {
         <h1 className='profile__title'>Привет, Виталий!</h1>
         <form className='profile__form' name='profile'>
           <label className='profile__label'>
-            <span className='profile__input-description'></span>
+            <span className='profile__input-description'>Имя</span>
             <input
               className='profile__input'
               type='text'
@@ -24,10 +24,10 @@ const Profile = () => {
               required
               onChange={() => {}}
             />
-            <span className='profile__span-error'></span>
           </label>
+          <span className='profile__span-error'>Что-то пошло не так... </span>
           <label className='profile__label'>
-            <span className='profile__input-description'></span>
+            <span className='profile__input-description'>E-mail</span>
             <input
               className='profile__input'
               type='email'
@@ -37,8 +37,8 @@ const Profile = () => {
               required
               onChange={() => {}}
             />
-            <span className='profile__span-error'></span>
           </label>
+          <span className='profile__span-error'>Что-то пошло не так...</span>
           <div className='profile__btn-container'>
             {isReadyToSave ? (
               <>
@@ -49,7 +49,7 @@ const Profile = () => {
                   className='profile__submit-btn'
                   type='submit'
                   onClick={() => {}}
-                ></button>
+                >Сохранить</button>
               </>
             ) : (
               <>
@@ -57,12 +57,12 @@ const Profile = () => {
                   className='profile__edit-btn'
                   type='button'
                   onClick={() => {}}
-                ></button>
+                >Редактировать</button>
                 <button
                   className='profile__logout-btn'
                   type='button'
                   onClick={() => {}}
-                ></button>
+                >Выйти из аккаунта</button>
               </>
             )}
           </div>
