fe5c7403573316d2cd5e4462203cdbaa5216c4b8
jsapro <77.3.77@mail.ru>
2023-08-19 21:08:37 +0300

    add Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,73 @@
 import './Profile.css';
+import Header from '../Header/Header';
 
 const Profile = () => {
+  const isReadyToSave = true;
+  // const isReadyToSave = false;
+
   return (
     <>
-      <div>Profile is imported !!!!!</div>
+      <Header />
+
+      <main className='profile'>
+        <h1 className='profile__title'>Привет, Виталий!</h1>
+        <form className='profile__form' name='profile'>
+          <label className='profile__label'>
+            <span className='profile__input-description'></span>
+            <input
+              className='profile__input'
+              type='text'
+              name='name'
+              minLength='2'
+              maxLength='30'
+              placeholder=''
+              required
+              onChange={() => {}}
+            />
+            <span className='profile__span-error'></span>
+          </label>
+          <label className='profile__label'>
+            <span className='profile__input-description'></span>
+            <input
+              className='profile__input'
+              type='email'
+              name='email'
+              minLength='3'
+              placeholder=''
+              required
+              onChange={() => {}}
+            />
+            <span className='profile__span-error'></span>
+          </label>
+          <div className='profile__btn-container'>
+            {isReadyToSave ? (
+              <>
+                <p className='profile__submit-error'>
+                  При обновлении профиля произошла ошибка.
+                </p>
+                <button
+                  className='profile__submit-btn'
+                  type='submit'
+                  onClick={() => {}}
+                ></button>
+              </>
+            ) : (
+              <>
+                <button
+                  className='profile__edit-btn'
+                  type='button'
+                  onClick={() => {}}
+                ></button>
+                <button
+                  className='profile__logout-btn'
+                  type='button'
+                  onClick={() => {}}
+                ></button>
+              </>
+            )}
+          </div>
+        </form>
+      </main>
     </>
   );
 };
