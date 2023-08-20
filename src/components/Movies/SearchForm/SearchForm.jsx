b2263a39644206705b6ee6542092f098a6f5fbe8
jsapro <77.3.77@mail.ru>
2023-08-20 19:19:51 +0300

    add structure to Navigation component

--- src/components/Movies/SearchForm/SearchForm.jsx
+++ src/components/Movies/SearchForm/SearchForm.jsx
@@ -6,7 +6,12 @@ const SearchForm = () => {
     <section className='search-form'>
       <form className='search-form__form'>
         <div className='search-form__container'>
-          <input className='search-form__input' placeholder='Фильм' type='text' />
+          <input
+            className='search-form__input'
+            placeholder='Фильм'
+            type='text'
+            required
+          />
           <button className='search-form__button' type='submit'></button>
         </div>
         <FilterCheckbox />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,70 @@
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
-  return (
-    <nav className='navigation'>
-      <ul className='navigation__list'>
-        <li className='navigation__register'>Регистрация</li>
-        <li className='navigation__login'>Войти</li>
-      </ul>
-    </nav>
-  );
+  const isMobileMode = window.innerWidth <= 768;
+  console.log(isMobileMode);
+
+  const isAuthorized = true;
+  // const isAuthorized = false;
+
+  const getGuestNav = () => {
+    return (
+      <nav className='nav-guest'>
+        <ul className='nav-guest__list'>
+          <li className='nav-guest__register'>
+            <Link>Регистрация</Link>
+          </li>
+          <li className='nav-guest__login'>
+            <Link>Войти</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
+  const getDesktopNav = () => {
+    return (
+      <nav className='nav-desktop'>
+        <ul className='nav-desktop__list'>
+          <li className='nav-desktop__item'>
+            <NavLink className='nav-desktop__link'>Фильмы</NavLink>
+          </li>
+          <li className='nav-desktop__item'>
+            <NavLink className='nav-desktop__link'>Сохранённые фильмы</NavLink>
+          </li>
+          <li className='nav-desktop__item'>
+            <NavLink className='nav-desktop__link'>Аккаунт</NavLink>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
+  const getBurgerNav = () => {
+    return (
+      <>
+        <h1 className='nav-burger'>hello</h1>
+        <div>Burger</div>
+      </>
+    );
+  };
+
+  const getActualNav = () => {
+    if (!isAuthorized) return getGuestNav();
+    if (isMobileMode) return getBurgerNav();
+    return getDesktopNav();
+  };
+
+  return getActualNav();
+  // <>
+  //   {!isAuthorized ? (
+  //     getNotAuthorizedNav()
+  //   ) : (
+  //     <>{isMobileMode ? getBurgerNav() : getNotBurgerNav()}</>
+  //   )}
+  // </>
 };
 
 export default Navigation;
