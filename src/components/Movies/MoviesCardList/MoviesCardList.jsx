25926ad0a043ab5ad5c41c9d3d289a5f0aea6c49
jsapro <77.3.77@mail.ru>
2023-08-28 11:57:27 +0300

    add notification text under search

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,25 +1,64 @@
+import { useLocation } from 'react-router-dom';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 
-const MoviesCardList = ({ onSaveMovie, onDeleteMovie, filteredMoviesArray }) => {
+const MoviesCardList = ({
+  onSaveMovie,
+  onDeleteMovie,
+  filteredMoviesArray,
+  isShortMovies,
+}) => {
+  const location = useLocation();
+
+  const getSearchErrorText = () => {
+    if (location.pathname === '/movies' && filteredMoviesArray.length === 0) {
+      return 'Нужно ввести ключевое слово';
+    }
+    if (
+      location.pathname === '/saved-movies' &&
+      filteredMoviesArray.length === 0 &&
+      isShortMovies
+    ) {
+      return 'Пока нет сохранённых короткометражных фильмов';
+    }
+    if (
+      location.pathname === '/saved-movies' &&
+      filteredMoviesArray.length === 0
+    ) {
+      return 'Пока нет сохранённых фильмов';
+    }
+    return
+  };
 
   return (
     <>
+      {filteredMoviesArray.length === 0 ? (
+        <p className='search-error-text'>{getSearchErrorText()}</p>
+      ) : null}
       <ul className='movies-card-list'>
         {filteredMoviesArray.map((movie, i) => {
-          return <MoviesCard key={movie.id} movie={movie} onSaveMovie={onSaveMovie} onDeleteMovie={onDeleteMovie} />;
+          return (
+            <MoviesCard
+              key={movie.id}
+              movie={movie}
+              onSaveMovie={onSaveMovie}
+              onDeleteMovie={onDeleteMovie}
+            />
+          );
         })}
       </ul>
 
-      <div className='load-button'>
-        <button
-          className='load-button__more-btn'
-          type='button'
-          disabled={false}
-        >
-          Ещё
-        </button>
-      </div>
+      {location.pathname === '/movies' && filteredMoviesArray.length !== 0 ? (
+        <div className='load-button'>
+          <button
+            className='load-button__more-btn'
+            type='button'
+            disabled={false}
+          >
+            Ещё
+          </button>
+        </div>
+      ) : null}
     </>
   );
 };
--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -41,8 +41,9 @@ const SavedMovies = ({ combinedMoviesArray, onDeleteMovie }) => {
           onSearch={handleSubmitSearch}
           isShortMovies={isShortMovies}
           onCheck={handleCheckBox}
-        />
+          />
         <MoviesCardList
+          isShortMovies={isShortMovies}
           onDeleteMovie={onDeleteMovie}
           filteredMoviesArray={filteredMoviesArray}
         />
