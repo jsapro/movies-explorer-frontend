2df1b3bd6fcef5f2411215dce575a15193f464a2
jsapro <77.3.77@mail.ru>
2023-09-01 13:43:27 +0300

    fix navigation on page 404

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -1,12 +1,19 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Page404.css';
 
 const Page404 = () => {
+  const navigate = useNavigate();
+  const getBack = () => {
+    navigate(-1);
+  };
+
   return (
     <main className='page404'>
       <h1 className='page404__title'>404</h1>
       <p className='page404__description'>Страница не найдена</p>
-      <Link className='page404__link' to='/'>Назад</Link>
+      <button className='page404__button' type="button" onClick={getBack}>
+        Назад
+      </button>
     </main>
   );
 };
